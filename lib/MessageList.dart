3266d6abf534bbaec4b23492d48b7a00c15b8d0a
Kye Yeung <Kye.Yeung@sainsburys.co.uk>
2019-05-15 14:21:26 +0100

    E14 done

--- lib/MessageList.dart
+++ lib/MessageList.dart
@@ -3,6 +3,7 @@ import 'package:emailapp/Message.dart';
 import 'package:emailapp/MessageCompose.dart';
 import 'package:emailapp/MessageDetail.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MessageList extends StatefulWidget {
   final String title;
@@ -44,6 +45,69 @@ class _MessageListState extends State<MessageList> {
             },
           )
         ],
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountEmail: Text("yeung.kye@pm.me"),
+              accountName: Text("Kye Yeung"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://avatars0.githubusercontent.com/u/7465872?s=460&v=4"),
+              ),
+              otherAccountsPictures: <Widget>[
+                GestureDetector(
+                    onTap: () {
+                      showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: Text("Adding new acount"),
+                            );
+                          });
+                    },
+                    child: CircleAvatar(
+                      child: Icon(Icons.add),
+                    ))
+              ],
+            ),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.inbox),
+              title: Text("Inbox"),
+              trailing: Chip(
+                label: Text("11", style: TextStyle(fontWeight: FontWeight.bold),),
+                backgroundColor:  Colors.blue,
+              ),
+            ),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.edit),
+              title: Text("Draft"),
+            ),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.archive),
+              title: Text("Archive"),
+            ),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.paperPlane),
+              title: Text("Sent"),
+            ),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.trash),
+              title: Text("Trash"),
+            ),
+            Divider(),
+            Expanded(
+              child: Align(
+                alignment: FractionalOffset.bottomCenter,
+                child: ListTile(
+                  leading: Icon(FontAwesomeIcons.cog),
+                  title: Text("Settings"),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
       body: FutureBuilder(
         future: future,
