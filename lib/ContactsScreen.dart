4758844367c54c39fae174598bc5cfaf1c84a282
Kye Yeung <Kye.Yeung@sainsburys.co.uk>
2019-05-26 18:08:40 +0100

    E18 done, StreamBuilder introduction

--- lib/ContactsScreen.dart
+++ lib/ContactsScreen.dart
@@ -1,20 +1,51 @@
 import 'package:emailapp/AppDrawer.dart';
+import 'package:emailapp/ContactManager.dart';
 import 'package:flutter/material.dart';
 
 class ContactsScreen extends StatelessWidget {
+  ContactManager manager = ContactManager();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Contacts")
-      ),
-      drawer: AppDrawer(),
-      body: Center(
-        child: Text("Contcts"),
-      ),
-
-
+    return DefaultTabController(
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text("Contacts"),
+            actions: <Widget>[
+              Chip(
+                label: StreamBuilder<int>(
+                  stream: manager.contactCounter,
+                  builder: (context, snapshot) {
+                    return Text(
+                      (snapshot.data ?? 0).toString(),
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    );
+                  },
+                ),
+                backgroundColor: Colors.red,
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 16),
+              )
+            ],
+          ),
+          drawer: AppDrawer(),
+          body: StreamBuilder<List<String>>(
+            stream: manager.contactListNow,
+            builder: (context, snapshot) {
+              List<String> contacts = snapshot.data;
+              return ListView.separated(
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(contacts[index]),
+                    );
+                  },
+                  separatorBuilder: (context, index) => Divider(),
+                  itemCount: contacts.length);
+            },
+          )),
+      length: 2,
     );
   }
-
 }
