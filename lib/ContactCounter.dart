fdbbb443398d91e57a47706c4b87edbe79616867
Kye Yeung <Kye.Yeung@sainsburys.co.uk>
2019-06-04 13:10:52 +0100

    E22 done

---
+++ lib/ContactCounter.dart
@@ -0,0 +1,22 @@
+import 'package:emailapp/ContactManager.dart';
+import 'package:emailapp/Provider.dart';
+import 'package:flutter/material.dart';
+
+class ContactCounter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    ContactManager manager = Provider.of(context);
+    return Chip(
+      label: StreamBuilder<int>(
+        stream: manager.contactCounter,
+        builder: (context, snapshot) {
+          return Text(
+            (snapshot.data ?? 0).toString(),
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          );
+        },
+      ),
+      backgroundColor: Colors.red,
+    );
+  }
+}
