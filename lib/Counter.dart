7a48d3328412db17599eb773582f8ffa06a54121
Kye Yeung <Kye.Yeung@sainsburys.co.uk>
2019-06-11 13:09:40 +0100

    E27 done

---
+++ lib/Counter.dart
@@ -0,0 +1,18 @@
+import 'package:emailapp/CounterManager.dart';
+import 'package:emailapp/Provider.dart';
+import 'package:flutter/material.dart';
+
+class Counter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    CounterManager manager = Provider.of(context).fetch(CounterManager);
+    return Center(
+        child: StreamBuilder<int>(
+      initialData: 0,
+      stream: manager.counter$,
+      builder: (context, snapshot) {
+        return Text("Calendar: ${snapshot.data}");
+      },
+    ));
+  }
+}
---
+++ lib/FabButton.dart
@@ -0,0 +1,12 @@
+import 'package:emailapp/CounterManager.dart';
+import 'package:emailapp/Provider.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FabButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    CounterManager manager = Provider.of(context).fetch(CounterManager);
+    return FloatingActionButton(onPressed: manager.increment);
+  }
+}
