3a83ac45f564ec6c2b4a0f76e2b5cb72700e1b25
Kye Yeung <Kye.Yeung@sainsburys.co.uk>
2019-06-18 12:58:34 +0100

    E33

--- lib/ContactManager.dart
+++ lib/ContactManager.dart
@@ -7,7 +7,6 @@ import 'package:emailapp/service/ContactService.dart';
 import 'package:rxdart/rxdart.dart';
 
 class ContactManager {
-
   /**
    * RX extentions
    * Stream = dart, Observable is the RX abstraction, same thing
@@ -19,20 +18,26 @@ class ContactManager {
 
   // PublishSubject: can subscribe to streams multiple times, a regular StreamController only once.
   final PublishSubject<String> _filterSubject = PublishSubject<String>();
+
   // BehaviorSubject: small memory, can remember last seen value. No waiting!
   final PublishSubject<int> _countSubject = PublishSubject<int>();
-  final PublishSubject<List<Contact>> _collectionSubject = PublishSubject<List<Contact>>();
-
+  final PublishSubject<List<Contact>> _collectionSubject =
+      PublishSubject<List<Contact>>();
 
   // 1. manager only receives data through this sink
   Sink<String> get inFilter => _filterSubject.sink;
+
   // 2. exposes stream 1 to provide data
   Observable<List<Contact>> get browse$ => _collectionSubject.stream;
+
   // 3. exposes stream 2 to provide data
   Observable<int> get count$ => _countSubject.stream;
 
   ContactManager() {
-    _filterSubject.listen((filter) async {
+    _filterSubject
+        // back pressure mechanism to slow down the number of events going to the ContactService.
+        .debounceTime(Duration(milliseconds: 500))
+        .listen((filter) async {
       // react to the stream/sink but initiating a browse then adding to another
       // stream _collectionSubject
       var contacts = await ContactService.browse(filter: filter);
@@ -43,7 +48,7 @@ class ContactManager {
     _collectionSubject.listen((list) => _countSubject.add(list.length));
   }
 
-  void dispose(){
+  void dispose() {
     _countSubject.close();
     _filterSubject.close();
   }
